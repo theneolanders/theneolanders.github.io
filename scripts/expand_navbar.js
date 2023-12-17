7a67d32a20d74c4005080c76b03529940a2ca67d
alizardguy <coyotehelp123@gmail.com>
2023-12-17 01:29:23 -0800

    Update expand_navbar.js

--- scripts/expand_navbar.js
+++ scripts/expand_navbar.js
@@ -1,8 +1,15 @@
+// get navbar
 const nav = document.getElementById('navbar');
 
+// close on start
 nav.classList.add('closed');
 
+// toggle navbar
 function toggleNavbar()
 {
     nav.classList.toggle('closed');
 }
+
+//todo 
+// animation
+// remember the state of the navbar between pages with localstorage
