0fb65cda2eb52d48baa0d6a5824571ecad351c0b
alizardguy <coyotehelp123@gmail.com>
2023-12-17 10:46:53 -0800

    Persistent navbar

--- scripts/expand_navbar.js
+++ scripts/expand_navbar.js
@@ -1,13 +1,22 @@
 // get navbar
 const nav = document.getElementById('navbar');
 
-// close on start
-nav.classList.add('closed');
+// set state
+if (localStorage.getItem("navClosed") == "true")
+{
+    nav.classList.add('closed');
+}
+else
+{
+    nav.classList.remove('closed');
+
+}
 
 // toggle navbar
 function toggleNavbar()
 {
     nav.classList.toggle('closed');
+    localStorage.setItem("navClosed", nav.classList.contains('closed'));
 }
 
 //todo 
