e4e6a55b6bd2a154f0bf50d45343d451bec7fcc4
alizardguy <coyotehelp123@gmail.com>
2024-01-20 11:57:28 -0800

    Update custom_header.js

--- scripts/custom_header.js
+++ scripts/custom_header.js
@@ -1,18 +1,18 @@
 /* custom navbar element */
 class MyNavbar extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = `
+        this.innerHTML = 
+        `
         <!--header-->
         <link rel="stylesheet" href="./styles/navigation.css">
-        <nav id="navbar">
+        <nav id="navbar" class="navList">
         <ul>  
           <li class="ringholder"><a href="#" class="ringSpin" onclick="toggleNavbar(); event.preventDefault();" aria-label="Home"></a></li>
           <li class="item"><a href="https://www.theneolanders.com/">Home</a></li>
-          <li class="item"><a href="./members/index.html">Members</a></li>
-          <li class="item"><a href="./portfolio/index.html">Group Portfolio</a></li>
-          <li class="item"><a href="./info/index.html">Info</a></li>
+          <li class="item"><a href="https://www.theneolanders.com/portfolio/">Group Portfolio</a></li>
+          <li class="item"><a href="https://www.theneolanders.com/info/">Info</a></li>
         </ul>
-      </nav>
+      </nav> 
         `;
     };
 };
