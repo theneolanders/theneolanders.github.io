7af96a283e50ded953cb8c851ebe7b4987b30ab5
alizard <42082829+alizardguy@users.noreply.github.com>
2024-10-26 17:52:24 -0700

    need to fix stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,10 @@
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPassthroughCopy("./src/style/");
     eleventyConfig.addPassthroughCopy("./src/scripts/");
-    eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy("./src/media");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
-    return {
-        dir: {
-            input: 'src',
-            includes: '_includes',
-            ouput: '_site'
-        },
-        templateFormats: ['md', 'njk', 'html'],
-        markdownTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-    }
-}
+  };
+  
+  
