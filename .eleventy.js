0cf61993f0371668cc5ef98595acc10854fb43bc
alizard <42082829+alizardguy@users.noreply.github.com>
2024-10-27 19:32:17 -0700

    running

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
-export default function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPassthroughCopy("./src/style/");
     eleventyConfig.addPassthroughCopy("./src/scripts/");
     eleventyConfig.addPassthroughCopy("./src/media");
     eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
-  };
+    return {
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            ouput: '_site'
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+    }
+};
   
   
