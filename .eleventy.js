c317258edf93208b90936eebc041921656658699
alizard <42082829+alizardguy@users.noreply.github.com>
2024-11-19 18:27:49 -0800

    404 test 1

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/404.md");
     eleventyConfig.addWatchTarget("./src/assets");
     return {
         dir: {
